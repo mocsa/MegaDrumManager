8735aab79519cad81eef037b7ae624a6901e87aa
Dmitri Skachkov <Dmitri Skachkov@c6631e87-15a2-f043-a34c-31ef9a3ea362>
2011-01-02 22:24:52 +0000

    git-svn-id: https://localhost/svn/Repositories/MegaDrumManager@644 c6631e87-15a2-f043-a34c-31ef9a3ea362

---
+++ src/gui/ControlsCurves.java
@@ -0,0 +1,287 @@
+package gui;
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+import javax.swing.JPanel;
+import java.awt.Dimension;
+import com.jgoodies.forms.layout.FormLayout;
+import com.jgoodies.forms.layout.ColumnSpec;
+import com.jgoodies.forms.layout.RowSpec;
+import com.jgoodies.forms.factories.FormFactory;
+import javax.swing.JSpinner;
+import java.awt.Font;
+import javax.swing.SpinnerNumberModel;
+import javax.swing.event.ChangeListener;
+import javax.swing.event.ChangeEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionAdapter;
+import javax.swing.JButton;
+import java.awt.Insets;
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
+import java.awt.event.ItemListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class ControlsCurves extends JPanel {
+	private CurvesPaint paintPanel;
+	//private int [] yValues = {2, 32, 64, 96, 128, 160, 192, 224, 255};
+	private ChangeListener spinnerChangeListener;
+	private JSpinner [] spinners;
+	private ConfigCurve [] configCurves;
+	private int curvePointer;
+	private int prevCurvePointer;
+	private JComboBox comboBox_curveNumber;
+	private JButton button_first;
+	private JButton button_prev;
+	private JButton button_next;
+	private JButton button_last;
+	private JButton button_get;
+	private JButton button_send;
+	private JButton button_getAll;
+	private JButton button_sendAll;
+	
+
+	/**
+	 * Create the panel.
+	 */
+	public ControlsCurves() {
+		configCurves = new ConfigCurve[Constants.CURVES_COUNT];
+        for(int i=0; i<Constants.CURVES_COUNT; i++){
+        	configCurves[i] = new ConfigCurve();
+        }
+        curvePointer = 0;
+        prevCurvePointer = -1;
+        
+		setLayout(new FormLayout(new ColumnSpec[] {
+				ColumnSpec.decode("left:281px:grow"),},
+			new RowSpec[] {
+				RowSpec.decode("12dlu"),
+				RowSpec.decode("1dlu"),
+				RowSpec.decode("12dlu"),
+				RowSpec.decode("279px"),
+				FormFactory.NARROW_LINE_GAP_ROWSPEC,
+				RowSpec.decode("12dlu:grow"),}));
+		
+		paintPanel = new CurvesPaint();
+		paintPanel.yValues = configCurves[curvePointer].yValues;
+		paintPanel.addMouseMotionListener(new MouseMotionAdapter() {
+			@Override
+			public void mouseDragged(MouseEvent arg0) {
+				updateYvalues();
+			}
+		});
+		paintPanel.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mousePressed(MouseEvent arg0) {
+				updateYvalues();
+			}
+		});
+		
+		JPanel panel = new JPanel();
+		add(panel, "1, 1, fill, fill");
+		panel.setLayout(new FormLayout(new ColumnSpec[] {
+				ColumnSpec.decode("24dlu"),
+				ColumnSpec.decode("1dlu"),
+				ColumnSpec.decode("24dlu"),
+				ColumnSpec.decode("1dlu"),
+				ColumnSpec.decode("24dlu"),
+				ColumnSpec.decode("1dlu"),
+				FormFactory.DEFAULT_COLSPEC,},
+			new RowSpec[] {
+				RowSpec.decode("12dlu"),}));
+		
+		button_get = new JButton("Get");
+		button_get.setMargin(new Insets(1, 4, 1, 4));
+		button_get.setFont(new Font("Segoe UI", Font.PLAIN, 9));
+		panel.add(button_get, "1, 1");
+		
+		button_send = new JButton("Send");
+		button_send.setMargin(new Insets(1, 4, 1, 4));
+		button_send.setFont(new Font("Segoe UI", Font.PLAIN, 9));
+		panel.add(button_send, "3, 1");
+		
+		button_getAll = new JButton("GetAll");
+		button_getAll.setMargin(new Insets(1, 4, 1, 4));
+		button_getAll.setFont(new Font("Segoe UI", Font.PLAIN, 9));
+		panel.add(button_getAll, "5, 1");
+		
+		button_sendAll = new JButton("SendAll");
+		button_sendAll.setMargin(new Insets(1, 4, 1, 4));
+		button_sendAll.setFont(new Font("Segoe UI", Font.PLAIN, 9));
+		panel.add(button_sendAll, "7, 1");
+		
+		JPanel panel_1 = new JPanel();
+		add(panel_1, "1, 3, fill, fill");
+		panel_1.setLayout(new FormLayout(new ColumnSpec[] {
+				FormFactory.RELATED_GAP_COLSPEC,
+				FormFactory.DEFAULT_COLSPEC,
+				FormFactory.RELATED_GAP_COLSPEC,
+				ColumnSpec.decode("20dlu"),
+				FormFactory.RELATED_GAP_COLSPEC,
+				ColumnSpec.decode("24dlu"),
+				ColumnSpec.decode("2dlu"),
+				ColumnSpec.decode("24dlu"),
+				ColumnSpec.decode("2dlu"),
+				ColumnSpec.decode("24dlu"),
+				ColumnSpec.decode("2dlu"),
+				ColumnSpec.decode("24dlu"),},
+			new RowSpec[] {
+				RowSpec.decode("fill:12dlu"),}));
+		
+		JLabel lblCurve = new JLabel("Curve");
+		lblCurve.setFont(new Font("Segoe UI", Font.BOLD, 12));
+		panel_1.add(lblCurve, "2, 1, right, fill");
+		
+		comboBox_curveNumber = new JComboBox();
+		comboBox_curveNumber.addItemListener(new ItemListener() {
+			public void itemStateChanged(ItemEvent arg0) {
+		        if (arg0.getStateChange() == ItemEvent.SELECTED) {
+		        	prevCurvePointer = curvePointer;
+		        	curvePointer = comboBox_curveNumber.getSelectedIndex();
+		        	updateControls();
+		        }
+			}
+		});
+		for (int i = 0; i < 4; i++) {
+			comboBox_curveNumber.addItem(i+1);
+		}
+		comboBox_curveNumber.setSelectedIndex(0);
+		comboBox_curveNumber.setMaximumRowCount(28);
+		comboBox_curveNumber.setFont(new Font("Segoe UI", Font.PLAIN, 10));
+		panel_1.add(comboBox_curveNumber, "4, 1, fill, fill");
+		
+		button_first = new JButton("first");
+		button_first.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+	        	prevCurvePointer = curvePointer;
+	        	curvePointer = 0;
+	        	updateControls();
+			}
+		});
+		button_first.setMargin(new Insets(1, 10, 1, 10));
+		button_first.setFont(new Font("Segoe UI", Font.PLAIN, 9));
+		panel_1.add(button_first, "6, 1");
+		
+		button_prev = new JButton("prev");
+		button_prev.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+	        	prevCurvePointer = curvePointer;
+	        	if (curvePointer > 0) {
+	        		curvePointer--;
+	        	}
+	        	updateControls();
+			}
+		});
+		button_prev.setMargin(new Insets(1, 8, 1, 8));
+		button_prev.setFont(new Font("Segoe UI", Font.PLAIN, 10));
+		panel_1.add(button_prev, "8, 1");
+		
+		button_next = new JButton("next");
+		button_next.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+	        	prevCurvePointer = curvePointer;
+	        	if (curvePointer < (Constants.CURVES_COUNT - 1)) {
+	        		curvePointer++;
+	        	}
+	        	updateControls();
+			}
+		});
+		button_next.setMargin(new Insets(1, 8, 1, 8));
+		button_next.setFont(new Font("Segoe UI", Font.PLAIN, 10));
+		panel_1.add(button_next, "10, 1");
+		
+		button_last = new JButton("last");
+		button_last.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+	        	prevCurvePointer = curvePointer;
+	        	curvePointer = Constants.CURVES_COUNT - 1;
+	        	updateControls();
+			}
+		});
+		button_last.setMargin(new Insets(1, 8, 1, 8));
+		button_last.setFont(new Font("Segoe UI", Font.PLAIN, 10));
+		panel_1.add(button_last, "12, 1");
+		add(paintPanel, "1, 4, left, fill");
+		
+		JPanel panelControls = new JPanel();
+		add(panelControls, "1, 6, fill, fill");
+		panelControls.setLayout(new FormLayout(new ColumnSpec[] {
+				ColumnSpec.decode("17dlu"),
+				ColumnSpec.decode("17dlu"),
+				ColumnSpec.decode("17dlu"),
+				ColumnSpec.decode("17dlu"),
+				ColumnSpec.decode("17dlu"),
+				ColumnSpec.decode("17dlu"),
+				ColumnSpec.decode("17dlu"),
+				ColumnSpec.decode("17dlu"),
+				ColumnSpec.decode("17dlu"),},
+			new RowSpec[] {
+				RowSpec.decode("12dlu"),}));
+		
+		spinnerChangeListener = new ChangeListener() {
+			public void stateChanged(ChangeEvent arg0) {
+				int id;
+				id = ((CSpinner)arg0.getSource()).getIndex();
+				configCurves[curvePointer].yValues[id] = ((Integer)((CSpinner)arg0.getSource()).getValue());
+				//paintPanel.yValues = yValues;
+				paintPanel.repaint();
+			}
+		};
+		
+		spinners = new CSpinner[9];
+		for (int i = 0; i < 9; i++ ) {
+			spinners[i] = new CSpinner(i);
+			spinners[i].setModel(new SpinnerNumberModel(configCurves[curvePointer].yValues[i], 2, 255, 1));
+			spinners[i].addChangeListener(spinnerChangeListener);
+			panelControls.add(spinners[i], ((Integer)(i+1)).toString() + ", 1");
+		}
+		
+	}    
+
+	private void updateControls() {
+    	paintPanel.yValues = configCurves[curvePointer].yValues;
+    	comboBox_curveNumber.setSelectedIndex(curvePointer);
+    	updateYvalues();
+    	paintPanel.repaint();
+	}
+	
+	private void updateYvalues() {
+		//yValues = paintPanel.yValues;
+		if (spinners != null) {
+			for (int i = 0; i < 9; i++) {
+				spinners[i].setValue(configCurves[curvePointer].yValues[i]);
+			}
+		}
+	}
+	
+	public ConfigCurve getConfig(int curve_id) {
+		return configCurves[curve_id];
+	}
+	
+	public void setConfig(ConfigCurve config,int curve_id) {
+		configCurves[curve_id].copyVarsFrom(config);
+		updateYvalues();
+		paintPanel.repaint();
+	}
+
+	
+	public int getCurvePointer() {
+		return curvePointer;
+	}
+	public JButton getButton_get() {
+		return button_get;
+	}
+	public JButton getButton_send() {
+		return button_send;
+	}
+	public JButton getButton_getAll() {
+		return button_getAll;
+	}
+	public JButton getButton_sendAll() {
+		return button_sendAll;
+	}
+}
---
+++ src/gui/ControlsMisc.java
@@ -0,0 +1,232 @@
+package gui;
+
+import javax.swing.JPanel;
+import com.jgoodies.forms.layout.FormLayout;
+import com.jgoodies.forms.layout.ColumnSpec;
+import com.jgoodies.forms.layout.RowSpec;
+import com.jgoodies.forms.factories.FormFactory;
+import javax.swing.JLabel;
+import java.awt.FlowLayout;
+import javax.swing.JButton;
+import java.awt.Font;
+import javax.swing.JSpinner;
+import javax.swing.JCheckBox;
+import javax.swing.SpinnerNumberModel;
+import javax.swing.event.ChangeListener;
+import javax.swing.event.ChangeEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.Insets;
+
+public class ControlsMisc extends JPanel {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 8689919340566654881L;
+
+	private ConfigMisc configMisc;
+	
+	private JSpinner spinner_noteoff;
+	private JSpinner spinner_pressroll;
+	private JSpinner spinner_latency;
+	private JCheckBox checkBox_bigVuMeter;
+	private JCheckBox checkBox_quickAccess;
+	private JCheckBox checkBox_altFalseTrSupp;
+	private JCheckBox checkBox_inputsPriority;
+	private JCheckBox checkBox_allGainsLow;
+	private JButton btnGet;
+	private JButton btnSend;
+
+	/**
+	 * Create the panel.
+	 */
+	public ControlsMisc() {
+		configMisc = new ConfigMisc();
+		setLayout(new FormLayout(new ColumnSpec[] {
+				ColumnSpec.decode("max(89dlu;default):grow"),},
+			new RowSpec[] {
+				RowSpec.decode("top:12dlu"),
+				RowSpec.decode("default:grow"),
+				RowSpec.decode("1dlu"),}));
+		
+		JPanel panel_buttons = new JPanel();
+		add(panel_buttons, "1, 1, fill, fill");
+		panel_buttons.setLayout(new FormLayout(new ColumnSpec[] {
+				ColumnSpec.decode("24dlu"),
+				ColumnSpec.decode("1dlu"),
+				ColumnSpec.decode("24dlu"),},
+			new RowSpec[] {
+				RowSpec.decode("12dlu"),}));
+		
+		btnGet = new JButton("Get");
+		btnGet.setMargin(new Insets(1, 4, 1, 4));
+		btnGet.setFont(new Font("Segoe UI", Font.PLAIN, 10));
+		panel_buttons.add(btnGet, "1, 1, fill, fill");
+		
+		btnSend = new JButton("Send");
+		btnSend.setMargin(new Insets(1, 4, 1, 4));
+		btnSend.setFont(new Font("Segoe UI", Font.PLAIN, 10));
+		panel_buttons.add(btnSend, "3, 1, fill, fill");
+		
+		JPanel panel = new JPanel();
+		add(panel, "1, 2, fill, fill");
+		panel.setLayout(new FormLayout(new ColumnSpec[] {
+				ColumnSpec.decode("right:56dlu"),
+				FormFactory.RELATED_GAP_COLSPEC,
+				ColumnSpec.decode("32dlu"),},
+			new RowSpec[] {
+				FormFactory.DEFAULT_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,}));
+		
+		JLabel lblNoteOffDelay = new JLabel("Note Off Delay");
+		lblNoteOffDelay.setFont(new Font("Segoe UI", Font.PLAIN, 10));
+		panel.add(lblNoteOffDelay, "1, 1");
+		
+		spinner_noteoff = new JSpinner();
+		spinner_noteoff.addChangeListener(new ChangeListener() {
+			public void stateChanged(ChangeEvent arg0) {
+				short value = ((Short)spinner_noteoff.getValue()).shortValue();
+				configMisc.note_off = (short)(value/10);
+				if (configMisc.note_off < configMisc.pressroll) {
+					configMisc.pressroll = configMisc.note_off; 
+				}
+				spinner_pressroll.setModel(new SpinnerNumberModel(new Short((short) (configMisc.pressroll*10)), new Short((short) 0), new Short((short) (configMisc.note_off*10)), new Short((short) 10)));
+			}
+		});
+		spinner_noteoff.setModel(new SpinnerNumberModel(new Short((short) 200), new Short((short) 100), new Short((short) 2000), new Short((short) 10)));
+		panel.add(spinner_noteoff, "3, 1");
+		
+		JLabel lblPressrollTimeout = new JLabel("Pressroll timeout");
+		lblPressrollTimeout.setFont(new Font("Segoe UI", Font.PLAIN, 10));
+		panel.add(lblPressrollTimeout, "1, 2");
+		
+		spinner_pressroll = new JSpinner();
+		spinner_pressroll.addChangeListener(new ChangeListener() {
+			public void stateChanged(ChangeEvent arg0) {
+				short value = ((Short)spinner_pressroll.getValue()).shortValue();
+				configMisc.pressroll = (short)(value/10);
+			}
+		});
+		spinner_pressroll.setModel(new SpinnerNumberModel(new Short((short) 0), new Short((short) 0), new Short((short) 200), new Short((short) 10)));
+		panel.add(spinner_pressroll, "3, 2");
+		
+		JLabel lblLatency = new JLabel("Latency");
+		lblLatency.setFont(new Font("Segoe UI", Font.PLAIN, 10));
+		panel.add(lblLatency, "1, 3");
+		
+		spinner_latency = new JSpinner();
+		spinner_latency.addChangeListener(new ChangeListener() {
+			public void stateChanged(ChangeEvent arg0) {
+				short value = ((Short)spinner_latency.getValue()).shortValue();
+				configMisc.latency = (short)value;
+			}
+		});
+		spinner_latency.setModel(new SpinnerNumberModel(new Short((short) 40), new Short((short) 10), new Short((short) 100), new Short((short) 1)));
+		panel.add(spinner_latency, "3, 3");
+		
+		JLabel lblBigVuMeter = new JLabel("Big VU meter");
+		lblBigVuMeter.setFont(new Font("Segoe UI", Font.PLAIN, 10));
+		panel.add(lblBigVuMeter, "1, 4");
+		
+		checkBox_bigVuMeter = new JCheckBox("");
+		checkBox_bigVuMeter.addChangeListener(new ChangeListener() {
+			public void stateChanged(ChangeEvent arg0) {
+				configMisc.big_vu_meter = checkBox_bigVuMeter.isSelected();
+			}
+		});
+		panel.add(checkBox_bigVuMeter, "3, 4");
+		
+		JLabel lblQuickAccess = new JLabel("Quick Access");
+		lblQuickAccess.setFont(new Font("Segoe UI", Font.PLAIN, 10));
+		panel.add(lblQuickAccess, "1, 5");
+		
+		checkBox_quickAccess = new JCheckBox("");
+		checkBox_quickAccess.addChangeListener(new ChangeListener() {
+			public void stateChanged(ChangeEvent arg0) {
+				configMisc.quick_access = checkBox_quickAccess.isSelected();
+			}
+		});
+		panel.add(checkBox_quickAccess, "3, 5");
+		
+		JLabel lblAltFalsetrsuppression = new JLabel("AltFalseTrSupp");
+		lblAltFalsetrsuppression.setFont(new Font("Segoe UI", Font.PLAIN, 10));
+		panel.add(lblAltFalsetrsuppression, "1, 6");
+		
+		checkBox_altFalseTrSupp = new JCheckBox("");
+		checkBox_altFalseTrSupp.addChangeListener(new ChangeListener() {
+			public void stateChanged(ChangeEvent arg0) {
+				configMisc.alt_false_tr_supp = checkBox_altFalseTrSupp.isSelected();
+			}
+		});
+		panel.add(checkBox_altFalseTrSupp, "3, 6");
+		
+		JLabel lblInputsPriority = new JLabel("Inputs Priority");
+		lblInputsPriority.setFont(new Font("Segoe UI", Font.PLAIN, 10));
+		panel.add(lblInputsPriority, "1, 7");
+		
+		checkBox_inputsPriority = new JCheckBox("");
+		checkBox_inputsPriority.addChangeListener(new ChangeListener() {
+			public void stateChanged(ChangeEvent arg0) {
+				configMisc.inputs_priority = checkBox_inputsPriority.isSelected();			}
+		});
+		panel.add(checkBox_inputsPriority, "3, 7");
+		
+		JLabel lblAllGainsLow = new JLabel("All Gains Low");
+		lblAllGainsLow.setFont(new Font("Segoe UI", Font.PLAIN, 10));
+		panel.add(lblAllGainsLow, "1, 8");
+		
+		checkBox_allGainsLow = new JCheckBox("");
+		checkBox_allGainsLow.addChangeListener(new ChangeListener() {
+			public void stateChanged(ChangeEvent arg0) {
+				configMisc.all_gains_low = checkBox_allGainsLow.isSelected();			}
+		});
+		panel.add(checkBox_allGainsLow, "3, 8");
+
+	}
+	
+	private void updateControls() {
+		spinner_noteoff.setValue((short)(configMisc.note_off*10));
+		spinner_pressroll.setModel(new SpinnerNumberModel(new Short((short) (configMisc.pressroll*10)),
+				new Short((short) 0),
+				new Short((short) (configMisc.note_off*10)),
+				new Short((short) 10)));
+		spinner_latency.setValue((short)(configMisc.latency));
+		checkBox_bigVuMeter.setSelected(configMisc.big_vu_meter);
+		checkBox_quickAccess.setSelected(configMisc.quick_access);
+		checkBox_altFalseTrSupp.setSelected(configMisc.alt_false_tr_supp);
+		checkBox_inputsPriority.setSelected(configMisc.inputs_priority);
+		checkBox_allGainsLow.setSelected(configMisc.all_gains_low);		
+	}
+	
+	public void setConfig(ConfigMisc config) {
+		configMisc = config;
+		updateControls();
+	}
+	
+	public ConfigMisc getConfig() {
+		return configMisc;
+	}
+
+	public JButton getBtnGet() {
+		return btnGet;
+	}
+	public JButton getBtnSend() {
+		return btnSend;
+	}
+
+	public void copyToConfigFull (ConfigFull config, int chain_id) {
+		config.sysex_misc = configMisc.getSysex(chain_id);
+	}
+	
+	public void loadFromConfigFull (ConfigFull config) {
+		configMisc.setFromSysex(config.sysex_misc);
+		updateControls();
+	}
+}
