dbbe6022fb56b833929890d66b71f760d95263f5
Dmitri Skachkov <d_skachkov@yahoo.com>
2012-10-07 00:13:58 +0100

    Release 20121007.
    1. Fixed a bug where Spin1023 controls would not get updated if the loaded value greater then 127.

--- src/gui/Spin1023Control.java
+++ src/gui/Spin1023Control.java
@@ -75,7 +75,7 @@ public class Spin1023Control extends JPanel {
 	}
 	
 	public void setValueWithoutEvents(int value) {
-		if ((value >= 0 ) && (value < 128)) {
+		if ((value >= 0 ) && (value < 1024)) {
 			if (firstSet) {
 				firstSet = false;
 			} else {
